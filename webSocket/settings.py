8e1e135b896ea7d4337616b2129aab57f55e8dac
Zicong Wang <actonnwang@gmail.com>
2023-12-12 02:42:15 -0500

    Update settings.py

--- webSocket/settings.py
+++ webSocket/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&0m9#b*!^*8p3gz3h7bu_)4y(=h6hyvlb&d(p3avupls)pm)se'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
